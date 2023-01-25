ecfeb361ebd7a11b01f3e6d4424b613f5eab57d6
khaled1gamal <112306279+khaled1gamal@users.noreply.github.com>
2023-01-25 09:00:10 +0300

    Add files via upload

---
+++ mine.js
@@ -0,0 +1,42 @@
+let inputColor = document.getElementById("input-color");
+let input1 = document.getElementById("input1").value;
+let input2 = document.getElementById("input2").value;
+let input3 = document.getElementById("input3").value;
+
+const toChoose = () => {
+  let wrong = document.getElementById("wrong");
+  let inputColor = document.getElementById("input-color");
+  let input1 = document.getElementById("input1").value;
+  let input2 = document.getElementById("input2").value;
+  let input3 = document.getElementById("input3").value;
+  if (input1 > 255 || input2 > 255 || input3 > 255) {
+    wrong.classList.add("show");
+  } else {
+    wrong.classList.remove('show')
+    urColor.style.backgroundColor = `rgb(${input1}, ${input2}, ${input3})`;
+  }
+};
+
+inputColor.addEventListener("mousemove", () => {
+  toChoose();
+});
+
+inputColor.addEventListener("change", () => {
+  toChoose();
+});
+
+inputColor.addEventListener("input", () => {
+  toChoose();
+});
+
+// inputColor.addEventListener("mousemove", () => {
+//   toChoose();
+// });
+
+// inputColor.addEventListener("change", () => {
+//   toChoose();
+// });
+
+// inputColor.addEventListener("input", () => {
+//   toChoose();
+// });
